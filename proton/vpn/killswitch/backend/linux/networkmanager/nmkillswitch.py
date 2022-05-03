eefbf6e4172549e6c13c7b6072a3d37a808cf6a2
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2022-05-03 17:11:23 +0100

    Fully implement network manager killswitch

--- proton/vpn/killswitch/backend/linux/networkmanager/nmkillswitch.py
+++ proton/vpn/killswitch/backend/linux/networkmanager/nmkillswitch.py
@@ -1,4 +1,7 @@
+from proton.vpn.backend.linux.networkmanager.dbus import NetworkManagerBus
 from proton.vpn.killswitch import KillSwitch
+from proton.vpn.killswitch.enums import KillSwitchStateEnum
+
 
 from .constants import (KILLSWITCH_CONN_NAME, KILLSWITCH_DNS_PRIORITY_VALUE,
                         KILLSWITCH_INTERFACE_NAME, ROUTED_CONN_NAME,
@@ -6,7 +9,6 @@ from .constants import (KILLSWITCH_CONN_NAME, KILLSWITCH_DNS_PRIORITY_VALUE,
                         IPv4_DUMMY_ADDRESS_SUFFIX, IPv4_DUMMY_GATEWAY,
                         IPv6_DUMMY_ADDRESS, IPv6_DUMMY_ADDRESS_SUFFIX,
                         IPv6_DUMMY_GATEWAY)
-from proton.vpn.killswitch.enums import KillSwitchStateEnum
 
 
 class NMKillSwitch(KillSwitch):
@@ -19,14 +21,11 @@ class NMKillSwitch(KillSwitch):
 
     ipv4_dummy_addrs = IPv4_DUMMY_ADDRESS
     ipv4_dummy_gateway = IPv4_DUMMY_GATEWAY
+    ipv4_dummy_suffx = IPv4_DUMMY_ADDRESS_SUFFIX
+
     ipv6_dummy_addrs = IPv6_DUMMY_ADDRESS
     ipv6_dummy_gateway = IPv6_DUMMY_GATEWAY
-
-    def _determine_initial_state(self):
-        self.on()
-
-    def _setup_off(self, **kwargs):
-        self._disable_killswitch()
+    ipv6_dummy_suffx = IPv6_DUMMY_ADDRESS_SUFFIX
 
     def _on_disconnected(self, **kwargs):
         if (
@@ -42,7 +41,10 @@ class NMKillSwitch(KillSwitch):
         ) or self.state == KillSwitchStateEnum.OFF:
             self.__ensure_killswitch_is_not_running()
         else:
-            self.__ensure_killswitch_only_allows_specific_routes()
+            if "server_ip" not in kwargs:
+                raise RuntimeError("Missing server IP for kill switch")
+
+            self.__ensure_killswitch_only_allows_specific_routes(kwargs.get("server_ip"))
 
     def _on_connected(self, **kwargs):
         if self.state == KillSwitchStateEnum.ON and (self.permanent or not self.permanent):
@@ -63,25 +65,139 @@ class NMKillSwitch(KillSwitch):
         else:
             self.__ensure_killswitch_is_not_running()
 
+    def _setup_off(self):
+        self.__ensure_killswitch_is_not_running()
+
+    def _setup_killswitch(self):
+        self.__ensure_killswitch_is_running()
+
     def __ensure_killswitch_is_not_running(self):
-        pass
+        self.__attempt_to_delete_killswitch_connection(self.ks_interface_name)
+        self.__attempt_to_delete_killswitch_connection(self.routed_interface_name)
 
     def __ensure_killswitch_is_running(self):
-        pass
+        is_killswitch_running = False
+        attempts = 3
 
-    def __ensure_killswitch_only_allows_specific_routes(self):
-        pass
+        while attempts > 0:
+            conn = self._nm_bus.get_network_manager().search_for_connection(interface_name=KILLSWITCH_INTERFACE_NAME)
+            if not conn:
+                self.__activate_killswitch_connection()
+            else:
+                is_killswitch_running = True
+                break
+
+            attempts -= 1
+
+        if not is_killswitch_running:
+            raise RuntimeError("Unable to enable kill switch")
+
+    def __ensure_killswitch_only_allows_specific_routes(self, server_ip):
+        self.__activate_routed_killswitch_connection(server_ip)
 
     def __remove_killswitch_that_allows_specific_routes(self):
-        pass
+        self.__attempt_to_delete_killswitch_connection(self.routed_interface_name)
 
-    @classmethod
-    def _get_priority(cls) -> int:
-        return 100
+    def __attempt_to_delete_killswitch_connection(self, interface_name):
+        is_killswitch_running = True
+        attempts = 3
 
-    @classmethod
-    def _validate(cls):
-        return True
+        while attempts > 0:
+            conn = self._nm_bus.get_network_manager().search_for_connection(interface_name=interface_name)
+            if conn:
+                conn.delete_connection()
+            else:
+                is_killswitch_running = False
+                break
+
+            attempts -= 1
+
+        if is_killswitch_running:
+            raise RuntimeError("Unable to delete kill switch with interface {}".format(interface_name))
+
+    def __activate_killswitch_connection(self):
+        nmbus = NetworkManagerBus()
+        nm_settings = nmbus.get_network_manager_settings()
+        nm_settings.add_connection(self.__generate_killswitch_connection())
+
+    def __generate_killswitch_connection(self):
+        from proton.vpn.backend.linux.networkmanager.dbus import DbusConnection
+
+        ks_conn = DbusConnection()
+        ks_conn.settings.human_readable_id = self.ks_conn_name
+        ks_conn.settings.interface_name = self.ks_interface_name
+
+        ks_conn.ipv4.address_data = {"address": self.ipv4_dummy_addrs, "prefix": self.ipv4_dummy_suffx}
+        ks_conn.ipv4.addresses = [(self.ipv4_dummy_addrs, self.ipv4_dummy_suffx, self.ipv4_dummy_gateway)]
+        ks_conn.ipv4.method = "manual"
+        ks_conn.ipv4.dns = ["0.0.0.0"]
+        ks_conn.ipv4.dns_priority = KILLSWITCH_DNS_PRIORITY_VALUE
+        ks_conn.ipv4.gateway = self.ipv4_dummy_gateway
+        ks_conn.ipv4.ignore_auto_dns = True
+        ks_conn.ipv4.route_metric = 98
+
+        ks_conn.ipv6.address_data = {"address": self.ipv6_dummy_addrs, "prefix": self.ipv6_dummy_suffx}
+        ks_conn.ipv6.addresses = [(self.ipv6_dummy_addrs, self.ipv6_dummy_suffx, self.ipv6_dummy_gateway)]
+        ks_conn.ipv6.method = "manual"
+        ks_conn.ipv6.dns = ["::1"]
+        ks_conn.ipv6.dns_priority = KILLSWITCH_DNS_PRIORITY_VALUE
+        ks_conn.ipv6.gateway = self.ipv6_dummy_gateway
+        ks_conn.ipv6.ignore_auto_dns = True
+        ks_conn.ipv6.route_metric = 98
+
+        return ks_conn.generate_configuration()
+
+    def __activate_routed_killswitch_connection(self, server_ip):
+        nmbus = NetworkManagerBus()
+        nm_settings = nmbus.get_network_manager_settings()
+        nm_settings.add_connection(self.__generate_routed_killswitch_connection(server_ip))
+
+    def __generate_routed_killswitch_connection(self, server_ip):
+        import ipaddress
+        subnet_list = list(
+            ipaddress.ip_network(
+                '0.0.0.0/0'
+            ).address_exclude(ipaddress.ip_network(server_ip))
+        )
+        formatted_data = [
+                (
+                    route.split("/")[0],
+                    int(route.split("/")[1]),
+                    route.split("/")[0]
+                ) for route in [str(ipv4) for ipv4 in subnet_list]
+            ]
+        from proton.vpn.backend.linux.networkmanager.dbus import DbusConnection
+
+        ks_conn = DbusConnection()
+        ks_conn.settings.human_readable_id = self.routed_conn_name
+        ks_conn.settings.interface_name = self.routed_interface_name
+        ks_conn.ipv4.addresses = formatted_data
+        ks_conn.ipv4.method = "manual"
+        ks_conn.ipv4.dns = ["0.0.0.0"]
+        ks_conn.ipv4.dns_priority = KILLSWITCH_DNS_PRIORITY_VALUE
+        ks_conn.ipv4.gateway = self.ipv4_dummy_gateway
+        ks_conn.ipv4.ignore_auto_dns = True
+        ks_conn.ipv4.route_metric = 98
+
+        ks_conn.ipv6.address_data = {"address": self.ipv6_dummy_addrs, "prefix": self.ipv6_dummy_suffx}
+        ks_conn.ipv6.addresses = [(self.ipv6_dummy_addrs, self.ipv6_dummy_suffx, self.ipv6_dummy_gateway)]
+        ks_conn.ipv6.method = "manual"
+        ks_conn.ipv6.dns = ["::1"]
+        ks_conn.ipv6.dns_priority = KILLSWITCH_DNS_PRIORITY_VALUE
+        ks_conn.ipv6.gateway = self.ipv6_dummy_gateway
+        ks_conn.ipv6.ignore_auto_dns = True
+        ks_conn.ipv6.route_metric = 98
+
+        return ks_conn.generate_configuration()
+
+    @property
+    def _nm_bus(self):
+        try:
+            self.__nm_bus
+        except AttributeError:
+            self.__nm_bus = NetworkManagerBus()
+
+        return self.__nm_bus
 
     @staticmethod
     def ipv4_to_int(ipv4_addr):
@@ -90,93 +206,17 @@ class NMKillSwitch(KillSwitch):
 
     @staticmethod
     def ipv6_to_byte_list(ipv6_addr):
-        import dbus
         import ipaddress
+
+        import dbus
         return [
             dbus.Byte(n) for n in list(ipaddress.ip_address(ipv6_addr).packed)
         ]
 
-    def __activate_killswitch_connection(self):
-        from proton.vpn.backend.linux.networkmanager.dbus import \
-            NetworkManagerBus
-        nmbus = NetworkManagerBus()
-        nm_settings = nmbus.get_network_manager_settings()
-        try:
-            nm_settings.add_connection(self.__generate_killswitch_connection())
-        except: # noqa
-            return False
+    @classmethod
+    def _get_priority(cls) -> int:
+        return 100
 
+    @classmethod
+    def _validate(cls):
         return True
-
-    def __generate_killswitch_connection(self):
-        import uuid
-        import dbus
-
-        settings_connection = dbus.Dictionary({
-                    'type': 'dummy',
-                    'uuid': str(uuid.uuid1()),
-                    'id': KILLSWITCH_CONN_NAME,
-                    'interface-name': KILLSWITCH_INTERFACE_NAME})
-
-        addr_ipv4 = dbus.Dictionary({
-            'address': IPv4_DUMMY_ADDRESS,
-            'prefix': IPv4_DUMMY_ADDRESS_SUFFIX,
-        })
-
-        ipv4_addresses = dbus.Array(
-            [
-                NMKillSwitch.ipv4_to_int(IPv4_DUMMY_ADDRESS),
-                dbus.UInt32(IPv4_DUMMY_ADDRESS_SUFFIX),
-                NMKillSwitch.ipv4_to_int(IPv4_DUMMY_GATEWAY)
-            ], signature=dbus.Signature('u')
-        )
-        settings_ipv4 = dbus.Dictionary({
-            'address-data': dbus.Array([addr_ipv4], signature=dbus.Signature('a{sv}')),
-            'method': 'manual',
-            'dns-priority': KILLSWITCH_DNS_PRIORITY_VALUE,
-            'gateway': IPv4_DUMMY_GATEWAY,
-            'ignore-auto-dns': True,
-            'route-metric': 98,
-            'addresses': dbus.Array([ipv4_addresses], signature=dbus.Signature('au')),
-            'dns': dbus.Array(
-                [
-                    NMKillSwitch.ipv4_to_int("0.0.0.0")
-                ], signature=dbus.Signature('u')
-            ),
-        })
-
-        addr_ipv6 = dbus.Dictionary({
-            'address': IPv6_DUMMY_ADDRESS,
-            'prefix': IPv6_DUMMY_ADDRESS_SUFFIX,
-        })
-        addresses_ipv6 = dbus.Struct(
-            (
-                NMKillSwitch.ipv6_to_byte_list(IPv6_DUMMY_ADDRESS),
-                dbus.UInt32(IPv6_DUMMY_ADDRESS_SUFFIX),
-                NMKillSwitch.ipv6_to_byte_list(IPv6_DUMMY_GATEWAY)
-            )
-        )
-        settings_ipv6 = dbus.Dictionary({
-            'address-data': dbus.Array([addr_ipv6], signature=dbus.Signature('a{sv}')),
-            'method': 'manual',
-            'dns-priority': KILLSWITCH_DNS_PRIORITY_VALUE,
-            'gateway': IPv6_DUMMY_GATEWAY,
-            'ignore-auto-dns': True,
-            'route-metric': 98,
-            'addresses': dbus.Array([addresses_ipv6]),
-            'dns': dbus.Array(
-                [
-                    dbus.Array(
-                        NMKillSwitch.ipv6_to_byte_list("::1"), signature=dbus.Signature('y')
-                    )
-                ], signature=dbus.Signature("ay")
-            )
-        })
-
-        con = dbus.Dictionary({
-            'connection': settings_connection,
-            'ipv4': settings_ipv4,
-            'ipv6': settings_ipv6
-        })
-
-        return con
