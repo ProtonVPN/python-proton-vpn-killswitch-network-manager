285b5da0c3bfe8c81d76d8cf43073c7f613929b9
Pep Llaneras <josep.llaneras@proton.ch>
2022-05-18 14:28:09 +0200

    Refactor code

--- proton/vpn/killswitch/backend/linux/networkmanager/nmkillswitch.py
+++ proton/vpn/killswitch/backend/linux/networkmanager/nmkillswitch.py
@@ -1,7 +1,11 @@
+import logging
+
 from proton.vpn.killswitch import KillSwitch
 from proton.vpn.killswitch.backend.linux.networkmanager.killswitch_connection import (
     KillSwitchConfig, KillSwitchConnectionHandler)
 from proton.vpn.killswitch.exceptions import KillSwitchError
+
+logger = logging.getLogger(__name__)
 
 
 class NMKillSwitch(KillSwitch):
@@ -11,30 +15,16 @@ class NMKillSwitch(KillSwitch):
         self._ks_handler = KillSwitchConnectionHandler()
 
     def _enable(self, server_ip=None, **_):
-        if server_ip:
-            self.__update_with_server_ip(server_ip)
-        else:
-            self.__enable()
-
-    def _disable(self, **_):
-        self.__assert_killswitch_connection_exists()
-
-        if self._ks_handler.is_killswitch_connection_active():
-            self._ks_handler.remove()
-            self.__assert_killswitch_connection_does_not_exists()
-
-    def __enable(self):
-        self.__assert_killswitch_connection_does_not_exists()
-
         if not self._ks_handler.is_killswitch_connection_active():
             self._ks_handler.add()
-            self.__assert_killswitch_connection_exists()
+        if server_ip:
+            self._ks_handler.update(server_ip)
 
-    def __update_with_server_ip(self, server_ip, **_):
         self.__assert_killswitch_connection_exists()
 
-        if self._ks_handler.is_killswitch_connection_active():
-            self._ks_handler.update(server_ip)
+    def _disable(self, **_):
+        self._ks_handler.remove()
+        self.__assert_killswitch_connection_does_not_exists()
 
     def __assert_killswitch_connection_exists(self):
         if not self._ks_handler.is_killswitch_connection_active():
