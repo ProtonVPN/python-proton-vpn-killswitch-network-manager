76c5129d28ee2696f8ebc562b51077e32cca236b
Pep Llaneras <josep.llaneras@proton.ch>
2022-08-16 10:43:29 +0200

    Run dbus system bus in CI

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+if [[ "$CI" == "true" ]]
+then
+  sudo mkdir -p /var/run/dbus
+  sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+fi
 
-export $(dbus-launch)
 python3 -m pytest tests/integration/test_killswitch_connection.py
